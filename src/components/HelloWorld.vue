9c0438cefca249b56074333cac69a1c0ca1226fb
iwillberich <2389342251@qq.com>
2022-01-15 10:50:13 +0800

    vue模板1.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <img src="@/assets/logo.png" alt="">
+    <h1>欢迎使用vue_cli</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+<style scoped>
+  .hello {
+    text-align: center;
+  }
+</style>
